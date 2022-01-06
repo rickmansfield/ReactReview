ae9a346c60385c286e0e2ea20cd8cf1a62f0b806
rickmansfield <rmansfield@realpeoplesratings.com>
2022-01-06 09:19:08 -0600

    adds useState Hook Example js file.

---
+++ UseStateHookExample.js
@@ -0,0 +1,9 @@
+// _Example of a hook:_ __useState hook:__
+
+// In functional components, the useState hook lets us define a state for a component:
+
+function Person(props) {
+// We are declaring a state variable called name.
+// setName is a function to update/change the value of name
+let [name, setName] = useState('');
+}
